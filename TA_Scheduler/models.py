3c07e890de2b82fe7210c6455a4c7e6c93fee079
MiCavros <mcavros@uwm.edu>
2024-11-19 21:30:07 -0600

    Created Models for Users, Contact Info, Class, and Section. Unsure about lists of models

---
+++ TA_Scheduler/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+class User(models.Model):
+    id = models.IntegerField(primary_key=True)
+    userType = models.IntegerField
+    fName = models.CharField(max_length=50)
+    lName = models.CharField(max_length=50)
+    MidInit = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+
+class userPublicInfo(models.Model):
+    id = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    workPhone = models.IntegerField(max_length=11)
+    email = models.CharField(max_length=50)
+    officeHours = models.DateField
+
+class privateContactInfo(models.Model):
+    id = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    cellPhone = models.IntegerField(max_length=11)
+    address = models.CharField(max_length=50)
+
+class Class(models.Model):
+    id = models.IntegerField(primary_key=True)
+    location = models.CharField(max_length=50)
+    time = models.DateTimeField
+    ##labList =
+    ##TAList =
+    instructor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+class Section(models.Model):
+    sectionId = models.IntegerField(primary_key=True)
+    classId = models.ForeignKey(Class, on_delete=models.CASCADE, null=True)
+    TA = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
