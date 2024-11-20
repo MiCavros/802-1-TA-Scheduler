df529814b45c3d2ec6575bb250fe1612ab6c55e1
MiCavros <mcavros@uwm.edu>
2024-11-19 21:57:00 -0600

    Created Tests for Login Page

--- TA_Scheduler/tests.py
+++ TA_Scheduler/tests.py
@@ -4,7 +4,27 @@ from .models import User, userPublicInfo, userPrivateInfo, Class, Section
 class LoginTest(TestCase):
     def setUp(self):
         self.client = Client()
-        testUser = User(id=1, password=1234)
+        testUser = User(id=1, password="1234")
         testUserContact = userPublicInfo(id=1, email="testUser@uwm.edu")
         testUser.save()
         testUserContact.save()
+
+    def test_wrongPassword(self):
+        resp = self.client.post("/",{"email": "testUser@uwm.edu","password": "2222"},follow=True)
+        self.assertEqual(resp.context["message"],"Incorrect Password")
+        self.assertEqual(resp.status_code,400)
+
+    def test_noUser(self):
+        resp = self.client.post("/",{"email": "unknownUser@uwm.edu","password": "2222"},follow=True)
+        self.assertEqual(resp.context["message"], "No User with this Email")
+        self.assertEqual(resp.status_code, 400)
+
+    def test_loginSuccessful(self):
+        resp = self.client.post("/",{"email": "testUser@uwm.edu","password": "1234"},follow=True)
+        self.assertRedirects(resp, "/home")
+        self.assertEqual(resp.status_code,400)
+
+    def test_BlankEntry(self):
+        resp = self.client.post("/",{"email": "","password": ""},follow=True)
+        self.assertEqual(resp.context["message"],"Email and/or Password cannot be blank")
+        self.assertEqual(resp.status_code,400)
